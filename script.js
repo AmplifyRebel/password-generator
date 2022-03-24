87fbab2f7b24b5d176f3acada74cd5fcdcc23236
Amplify Rebel <erj2204@gmail.com>
2022-03-24 13:43:02 -0500

    added java script still need to fix the alert

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var password=document.getElementById("password");
-
- function genPassword() {
+ function genPassword(click) {
+    var userInput = prompt("How long do you want your password (8-128)");
+    var compOptions = [""];
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- }
+    var passwordLength= userInput
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber +1);
+     }
+           document.getElementById("password").value = password;
+    }
+ genPassword()
