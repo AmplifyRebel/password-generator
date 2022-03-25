f990fd873f3ec3001093bdc51b089473731364bf
Amplify Rebel <erj2204@gmail.com>
2022-03-24 23:02:53 -0500

    tried fixing the js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var password=document.getElementById("password");
+
+window.addEventListener("beforeunload", function(e){
+   e.preventDefault();
+}, false);
  function genPassword(click) {
     var userInput = prompt("How long do you want your password (8-128)");
-    var compOptions = [""];
+    var compOptions = prompt;
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passwordLength= userInput
     var password = "";
-    for (var i = 0; i <= passwordLength; i++) {
+    for (var i = 0; i < passwordLength; i++) {
       var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber +1);
+      password += chars.substring(randomNumber, randomNumber +1,);
      }
            document.getElementById("password").value = password;
     }
